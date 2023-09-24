178925218ce24e5d06e78f43a42625a00a931668
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-09-24 14:17:17 -0400

    Update fix-paths.ps1

--- dvlup/content/en/blog/fix-paths.ps1
+++ dvlup/content/en/blog/fix-paths.ps1
@@ -1,7 +1,1 @@
-function find_and_replace($find, $replace) {
-    (dir *.md -Recurse).FullName | ForEach-Object {
-        (Get-Content -Path $_ -Raw) -replace "$find", "$replace" |
-            Set-Content -Path $_ -NoNewline }
-}
-
-find_and_replace("wp-content/uploads", "dvlup-blog/wp-content/uploads")
+Get-ChildItem ".\blog" -Filter *.md | ForEach-Object { (Get-Content -Path $_ -Raw) -replace "/dvlup-blog/dvlup-blog/", "/dvlup-blog/" | Set-Content -Path $_ -NoNewline }
